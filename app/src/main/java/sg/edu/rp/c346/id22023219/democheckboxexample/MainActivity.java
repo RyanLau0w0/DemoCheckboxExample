2199bf641c62de407b2e59c2f0d342bbcf6bd164
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-11 11:21:55 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22023219/democheckboxexample/MainActivity.java
@@ -0,0 +1,25 @@
+package sg.edu.rp.c346.id22023219.democheckboxexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    CheckBox cbEnabled;
+    Button btnCheck;
+    TextView tvShow;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        cbEnabled = findViewById(R.id.checkBoxDiscount);
+        btnCheck = findViewById(R.id.buttonCheck);
+        tvShow = findViewById(R.id.textView);
+
+    }
+}
