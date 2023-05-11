51aaf5c7310af52b280b486bd24e23ebb95bf471
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-11 12:50:58 +0800

    Completed till debugging

--- app/src/main/java/sg/edu/rp/c346/id22023219/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023219/democheckboxexample/MainActivity.java
@@ -28,16 +28,34 @@ public class MainActivity extends AppCompatActivity {
         btnCheck.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v ) {
+                //Below is code for logcat
                 Log.i("MyActivity", "Inside onClick()");
-                String discount = "not given";
-                if (cbEnabled.isChecked()){
-                    discount = "given";
-                }
-                String stringResponse = "The discount is " + discount;
-                tvShow.setText(stringResponse);
+                //Below is code for textview
+                // String discount = "not given";
+                //if (cbEnabled.isChecked()){
+                    //discount = "given";
+                //}
+                //String stringResponse = "The discount is " + discount;
+                //tvShow.setText(stringResponse);
+                //Below is code for toast
                 Toast.makeText(MainActivity.this,"Button Click",
                 Toast.LENGTH_LONG).show();
+                //Below is code for calcPay
+                if(cbEnabled.isChecked()) {
+                    double pay = calcPay(100, 20);
+                    tvShow.setText("The discount is given. You need to pay " + pay);
+                }
+                else {
+                    double pay = calcPay(100, 0);
+                    tvShow.setText("The discount is not given. You need to pay " + pay);
+                }
             }
+
         });
+    }
+
+    private double calcPay(double price, double discount){
+        double pay = price * (1 - (discount/100));
+        return pay;
     }
 }
