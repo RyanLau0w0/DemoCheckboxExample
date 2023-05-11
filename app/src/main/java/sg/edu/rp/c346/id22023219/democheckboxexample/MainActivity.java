4a8103fbaa5d51e33397fe6056fcb79d401186c0
RyanLauOwO <22023219@myrp.edu.sg>
2023-05-11 12:06:04 +0800

    Completed till logcat

--- app/src/main/java/sg/edu/rp/c346/id22023219/democheckboxexample/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22023219/democheckboxexample/MainActivity.java
@@ -3,9 +3,12 @@ package sg.edu.rp.c346.id22023219.democheckboxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,5 +24,20 @@ public class MainActivity extends AppCompatActivity {
         btnCheck = findViewById(R.id.buttonCheck);
         tvShow = findViewById(R.id.textView);
 
+        //Here is code for check
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v ) {
+                Log.i("MyActivity", "Inside onClick()");
+                String discount = "not given";
+                if (cbEnabled.isChecked()){
+                    discount = "given";
+                }
+                String stringResponse = "The discount is " + discount;
+                tvShow.setText(stringResponse);
+                Toast.makeText(MainActivity.this,"Button Click",
+                Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
